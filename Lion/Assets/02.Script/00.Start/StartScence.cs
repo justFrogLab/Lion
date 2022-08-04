00af104f6df613817150846cd299dbcc7f508beb
ChangSik Kim <106213484+justFrogLab@users.noreply.github.com>
2022-08-04 18:00:41 +0900

    v0.0.4
    1. 페이드 인 효과

---
+++ Lion/Assets/02.Script/00.Start/StartScence.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class StartScence : MonoBehaviour
+{
+    [SerializeField]
+    Text logoTxt;
+
+    private void Start()
+    {
+        Debug.Log(logoTxt.color);
+        StartCoroutine(startLogoTxt());
+    }
+
+    IEnumerator startLogoTxt()
+    {
+        float f = 1.0f;
+        logoTxt.color = new Color(logoTxt.color.r, logoTxt.color.g, logoTxt.color.b, 1);
+
+        while (logoTxt.color.a > 0.0f)
+        {
+            logoTxt.color = new Color(logoTxt.color.r, logoTxt.color.g, logoTxt.color.b, logoTxt.color.a - (Time.deltaTime / 4.0f));
+            yield return null;
+        }
+    }
+}
